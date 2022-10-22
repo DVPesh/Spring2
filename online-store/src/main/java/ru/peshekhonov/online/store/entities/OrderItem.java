0775dc5dc406c5768129e4087e1af99e8a7b467a
dvpesh <dvpesh@mail.ru>
2022-10-22 15:58:26 +0300

    добавлены сущности Order и OrderItem

---
+++ online-store/src/main/java/ru/peshekhonov/online/store/entities/OrderItem.java
@@ -0,0 +1,47 @@
+package ru.peshekhonov.online.store.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor
+@Data
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price_per_product")
+    private BigDecimal pricePerProduct;
+
+    @Column(name = "cost")
+    private BigDecimal cost;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
