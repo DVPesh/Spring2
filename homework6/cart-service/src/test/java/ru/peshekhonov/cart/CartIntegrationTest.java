30fb1856d1162890fe7c9c9215a4198c8b7610ff
dvpesh <dvpesh@mail.ru>
2022-11-19 20:08:40 +0300

    добавлен тест уменьшения количества товара в корзине по HTTP

--- homework6/cart-service/src/test/java/ru/peshekhonov/cart/CartIntegrationTest.java
+++ homework6/cart-service/src/test/java/ru/peshekhonov/cart/CartIntegrationTest.java
@@ -27,6 +27,24 @@ public class CartIntegrationTest {
     @MockBean
     private ProductServiceIntegration productService;
 
+    @BeforeEach
+    public void setUp() {
+        ProductDto productWithId2 = new ProductDto();
+        productWithId2.setId(2L);
+        productWithId2.setTitle("бананы");
+        productWithId2.setPrice(BigDecimal.valueOf(98.35));
+        productWithId2.setCategoryTitle("пищевые продукты");
+
+        ProductDto productWithId5 = new ProductDto();
+        productWithId5.setId(5L);
+        productWithId5.setTitle("кукла");
+        productWithId5.setPrice(BigDecimal.valueOf(1500));
+        productWithId5.setCategoryTitle("детские товары");
+
+        Mockito.when(productService.findById(2L)).thenReturn(productWithId2);
+        Mockito.when(productService.findById(5L)).thenReturn(productWithId5);
+    }
+
     @Test
     @Order(1)
     public void cartControllerGetEmptyCartTest() {
@@ -42,21 +60,6 @@ public class CartIntegrationTest {
     public void cartControllerAddProductToCartTest() {
         ResponseEntity<CartDto> entity;
         HttpStatus status;
-
-        ProductDto productWithId2 = new ProductDto();
-        productWithId2.setId(2L);
-        productWithId2.setTitle("бананы");
-        productWithId2.setPrice(BigDecimal.valueOf(98.35));
-        productWithId2.setCategoryTitle("пищевые продукты");
-
-        ProductDto productWithId5 = new ProductDto();
-        productWithId5.setId(5L);
-        productWithId5.setTitle("кукла");
-        productWithId5.setPrice(BigDecimal.valueOf(1500));
-        productWithId5.setCategoryTitle("детские товары");
-
-        Mockito.when(productService.findById(2L)).thenReturn(productWithId2);
-        Mockito.when(productService.findById(5L)).thenReturn(productWithId5);
 
         status = restTemplate.getForEntity("/api/v1/cart/add/{productId}", Object.class, 2)
                 .getStatusCode();
@@ -95,5 +98,34 @@ public class CartIntegrationTest {
         Assertions.assertEquals(2L, (int) Objects.requireNonNull(entity.getBody()).getItems().get(1).getQuantity());
         Assertions.assertEquals(BigDecimal.valueOf(1500), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(1).getPricePerProduct());
         Assertions.assertEquals(BigDecimal.valueOf(3000), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(1).getCost());
+    }
+
+    @Test
+    @Order(3)
+    public void cartControllerSubtractProductFromCartTest() {
+        ResponseEntity<CartDto> entity;
+        HttpStatus status;
+
+        status = restTemplate.getForEntity("/api/v1/cart/subtract/{productId}", Object.class, 5)
+                .getStatusCode();
+        Assertions.assertSame(status, HttpStatus.OK);
+
+        entity = restTemplate.getForEntity("/api/v1/cart", CartDto.class);
+        Assertions.assertSame(entity.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(2, Objects.requireNonNull(entity.getBody()).getItems().size());
+
+        Assertions.assertEquals(BigDecimal.valueOf(98.35 + 1500), (BigDecimal) Objects.requireNonNull(entity.getBody()).getTotalCost());
+
+        Assertions.assertEquals(2L, (long) Objects.requireNonNull(entity.getBody()).getItems().get(0).getProductId());
+        Assertions.assertEquals("бананы", Objects.requireNonNull(entity.getBody()).getItems().get(0).getProductTitle());
+        Assertions.assertEquals(1L, (int) Objects.requireNonNull(entity.getBody()).getItems().get(0).getQuantity());
+        Assertions.assertEquals(BigDecimal.valueOf(98.35), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(0).getPricePerProduct());
+        Assertions.assertEquals(BigDecimal.valueOf(98.35), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(0).getCost());
+
+        Assertions.assertEquals(5L, (long) Objects.requireNonNull(entity.getBody()).getItems().get(1).getProductId());
+        Assertions.assertEquals("кукла", Objects.requireNonNull(entity.getBody()).getItems().get(1).getProductTitle());
+        Assertions.assertEquals(1L, (int) Objects.requireNonNull(entity.getBody()).getItems().get(1).getQuantity());
+        Assertions.assertEquals(BigDecimal.valueOf(1500), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(1).getPricePerProduct());
+        Assertions.assertEquals(BigDecimal.valueOf(1500), (BigDecimal) Objects.requireNonNull(entity.getBody()).getItems().get(1).getCost());
     }
 }
