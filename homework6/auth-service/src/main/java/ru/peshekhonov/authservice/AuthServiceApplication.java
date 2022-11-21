a7fe8f542da7ff026c328b89e15f68409a16b3d8
dvpesh <dvpesh@mail.ru>
2022-11-21 18:35:26 +0300

    Дополнение к практическому заданию №7 В микросервисе auth-service секрет для JWT хранится в отдельном файле свойств secret.properties. При запуске теста файл secret.properties не определяется, поэтому секрет хранится в файле тестового профиля application-test.yml.

--- homework6/auth-service/src/main/java/ru/peshekhonov/authservice/AuthServiceApplication.java
+++ homework6/auth-service/src/main/java/ru/peshekhonov/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package ru.peshekhonov.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "secret.properties", ignoreResourceNotFound = true)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
