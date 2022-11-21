ea824165fc99b84ce38b934102d5344176a6a384
dvpesh <dvpesh@mail.ru>
2022-11-21 03:49:09 +0300

    добавлены тесты для сервиса auth-service

---
+++ homework6/auth-service/src/main/resources/db/test/V1__init.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS visitors
+(
+    id         bigint       NOT NULL AUTO_INCREMENT,
+    username   varchar(100) NOT NULL,
+    password   varchar(255) NOT NULL,
+    email      varchar(50)       DEFAULT NULL,
+    created_at timestamp    NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp    NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE KEY username_UNIQUE (username),
+    UNIQUE KEY password_UNIQUE (password),
+    UNIQUE KEY email_UNIQUE (email)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='таблица пользователей';
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id         bigint      NOT NULL AUTO_INCREMENT,
+    name       varchar(50) NOT NULL,
+    created_at timestamp   NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp   NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE KEY name_UNIQUE (name)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='таблица ролей';
+
+CREATE TABLE IF NOT EXISTS visitors_roles
+(
+    visitor_id bigint    NOT NULL,
+    role_id    bigint    NOT NULL,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (visitor_id, role_id),
+    KEY fk_roles_idx (role_id),
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles (id),
+    CONSTRAINT fk_visitors FOREIGN KEY (visitor_id) REFERENCES visitors (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='таблица ролей пользователей';
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+insert into visitors (username, password, email)
+values ('Dima Petrov', '$2a$12$kHYybMz2P4hCtq7GW.wf3.LamFg338C48zvPdbudyzmxCN8LuQCei', 'dima@mail.ru'),
+       ('Oleg Ivanov', '$2a$12$G628fF.N.y/xcEUhUvF67u0nLfmeenasXnVaDG/ita2pgsCxIsQWK', 'oleg@mail.ru'),
+       ('Ann Kuskova', '$2a$12$zAY4wh7CXHkveqvPTUTuuOXql5tHYbt12V3ePphiU8EoSV38o8m4G', 'ann@mail.ru'),
+       ('Peter Vetrov', '$2a$12$myTKY4SMKfZ/6ldoUYYh7uqLt4Y8XgPtz1JbuYRHbGWbpKF3tEyte', 'peter@mail.ru');
+
+insert into visitors_roles (visitor_id, role_id)
+values (1, 1),
+       (1, 2),
+       (2, 1),
+       (3, 1);
--- homework6/auth-service/src/test/java/ru/peshekhonov/authservice/AuthServiceApplicationTests.java
+++ homework6/auth-service/src/test/java/ru/peshekhonov/authservice/AuthServiceApplicationTests.java
@@ -1,13 +1,69 @@
 package ru.peshekhonov.authservice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import ru.peshekhonov.authservice.utils.JwtTokenUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
+    @Test
+    void securityTokenTest() throws Exception {
+        String jsonRequest = """
+                {
+                    "username": "Dima Petrov",
+                    "password": "100"
+                }
+                """;
+        MvcResult result = mockMvc.perform(
+                        post("/auth")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+        token = token.replace("{\"token\":\"", "").replace("\"}", "");
+
+        Assertions.assertThat(jwtTokenUtil.getUsernameFromToken(token))
+                .isEqualTo("Dima Petrov");
+
+        Assertions.assertThat(jwtTokenUtil.getRoles(token))
+                .hasSize(2)
+                .contains("ROLE_USER", "ROLE_ADMIN");
+
+        jsonRequest = """
+                {
+                    "username": "Dima Petrov",
+                    "password": "200"
+                }
+                """;
+        mockMvc.perform(
+                        post("/auth")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
 }
